ddcd2f475e0934ba3b6f3f12b8d17e95f771e337
Kaung Si Thu <kgcthu.kev@gmail.com>
2022-05-09 19:49:30 +0630

    initial commit

---
+++ data.js
@@ -0,0 +1,83 @@
+const items = [
+  {
+    id: 1,
+    name: 'Sample Products with long name',
+    price: 54000,
+    image: 'https://dummyimage.com/600x400/000/fff&text=dummy+image',
+  },
+  {
+    id: 2,
+    name: 'Sample Products with long name',
+    price: 54000,
+    image: 'https://dummyimage.com/600x400/000/fff&text=dummy+image',
+  },
+  {
+    id: 3,
+    name: 'Sample Products with long name',
+    price: 54000,
+    image: 'https://dummyimage.com/600x400/000/fff&text=dummy+image',
+  },
+  {
+    id: 4,
+    name: 'Sample Products with long name',
+    price: 54000,
+    image: 'https://dummyimage.com/600x400/000/fff&text=dummy+image',
+  },
+  {
+    id: 5,
+    name: 'Sample Products with long name',
+    price: 54000,
+    image: 'https://dummyimage.com/600x400/000/fff&text=dummy+image',
+  },
+  {
+    id: 6,
+    name: 'Sample Products with long name',
+    price: 54000,
+    image: 'https://dummyimage.com/600x400/000/fff&text=dummy+image',
+  },
+]
+
+const promotionItems = [
+  {
+    id: 1,
+    name: 'Sample Products with long name',
+    originalPrice: 60000,
+    discountedPrice: 54000,
+    image: 'https://dummyimage.com/600x400/000/fff&text=dummy+image',
+  },
+  {
+    id: 2,
+    name: 'Sample Products with long name',
+    originalPrice: 60000,
+    discountedPrice: 54000,
+    image: 'https://dummyimage.com/600x400/000/fff&text=dummy+image',
+  },
+  {
+    id: 3,
+    name: 'Sample Products with long name',
+    originalPrice: 60000,
+    discountedPrice: 54000,
+    image: 'https://dummyimage.com/600x400/000/fff&text=dummy+image',
+  },
+  {
+    id: 4,
+    name: 'Sample Products with long name',
+    originalPrice: 60000,
+    discountedPrice: 54000,
+    image: 'https://dummyimage.com/600x400/000/fff&text=dummy+image',
+  },
+  {
+    id: 5,
+    name: 'Sample Products with long name',
+    originalPrice: 60000,
+    discountedPrice: 54000,
+    image: 'https://dummyimage.com/600x400/000/fff&text=dummy+image',
+  },
+  {
+    id: 6,
+    name: 'Sample Products with long name',
+    originalPrice: 60000,
+    discountedPrice: 54000,
+    image: 'https://dummyimage.com/600x400/000/fff&text=dummy+image',
+  },
+]
